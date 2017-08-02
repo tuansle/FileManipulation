3d3c494247640c76218245944270d0ee4812e08d
Tuan Le <tuan.le@geo.tuwien.ac.at>
2017-08-02 09:22:52 +0200

    update rename code: can rename s1 to asa and vice versa

--- eopdanube/rename_s1ssm_to_asassm.py
+++ eopdanube/rename_s1ssm_to_asassm.py
@@ -1,7 +1,19 @@
 import os
 
-folder_to_rename = '/home/tle/data/DATAPROCESSING/eopdanube_testmixssm/Envisat_ASAR/WS/products/datasets/ssm/C1002/EQUI7_EU500M/E048N012T6/'
+def batch_rename(folder, dst_name="S1"):
+    if dst_name == "S1":
+        for fil in os.listdir(folder):
+            if "_ASA" in fil:
+                os.rename(os.path.join(folder, fil),
+                          os.path.join(folder, fil.replace("ASAWS---M", "S1AIWGRDH")))
+    elif dst_name == "ASA":
+        for fil in os.listdir(folder):
+            if "_S1" in fil:
+                os.rename(os.path.join(folder, fil),
+                          os.path.join(folder, fil.replace("S1AIWGRDH", "ASAWS---M")))
+    else:
+        raise Exception("dst_name should be ASA or S1")
 
-for fil in os.listdir(folder_to_rename):
-    if "S1A" in fil:
-        os.rename(os.path.join(folder_to_rename,fil), os.path.join(folder_to_rename,fil.replace("S1AIWGRDH","ASAWS---M")))
+if __name__ == "__main__":
+    batch_rename(folder="/home/tle/data/DATAPROCESSING/eopdanube_testmixssm/Sentinel-1_CSAR/IWGRDH/parameters/datasets/par_stat/B0201/EQUI7_EU500M/E048N012T6",
+                 dst_name="S1")
